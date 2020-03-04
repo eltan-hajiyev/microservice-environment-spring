4b05cc55817a1376898dd80c1556a9d86ee3fc27
Eltan Hajiyev <eltanh@goldenpay.az>
2020-03-05 02:41:02 +0400

    service name changes

--- internal-service-payday/src/main/java/excludeautoscan/internal_service/MicroServiceName.java
+++ internal-service-payday/src/main/java/excludeautoscan/internal_service/MicroServiceName.java
@@ -5,6 +5,6 @@ public class MicroServiceName {
 	
 	public static final String AUTHORIZATION = "authorization";
 	public static final String ACCOUNT = "account";
-	public static final String MESSAGE_SERVICE = "message-service";
+	public static final String MESSAGE_SERVICE = "message";
 	public static final String LOCALIZATION_SERVICE = "localization-service";
 }
